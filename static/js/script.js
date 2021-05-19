2709e45977478a049a826b05c42cbca423248c2a
victor chen <vic4code@gmail.com>
2021-05-20 03:03:50 +0800

    Change to static website

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    // Handle submit event
+    // $('#submit').click(function() {
+    //     $(this).html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>Loading...').attr('disabled', true);
+    //     setTimeout(function() {
+    //         $("#form").submit();
+    //     }, 1000);
+    // });
+    
+});
